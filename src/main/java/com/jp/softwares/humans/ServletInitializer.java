721af6821049bf0ef6399ddfa3040f2992597fce
Andre-Luiz01 <andreff.net@gmail.com>
2024-02-27 18:56:59 -0300

    pastas criadas

---
+++ src/main/java/com/jp/softwares/humans/ServletInitializer.java
@@ -0,0 +1,11 @@
+package com.jp.softwares.humans;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HumansApplication.class);
+    }
+}
